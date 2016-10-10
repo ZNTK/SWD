6ba004bff5bec37de6ec840b25fcbaacd5ed6afe
rplo <rlo@tak.nie>
2016-10-10 21:07:02 +0200

    Walidacje - sprawdzenie czy wszystkie Celle mają wartość

---
+++ SWD/Services/ValidationService.cs
@@ -0,0 +1,22 @@
+﻿using SWD.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD.Services
+{
+    public static class ValidationService
+    {
+        public static bool TableIsValid(Model.Table table)
+        {
+            foreach (Row row in table.Rows)
+                foreach (Cell cell in row.Cells)
+                    if (String.IsNullOrEmpty(cell.Value.Replace(" ", String.Empty)))
+                        return false;
+
+            return true;
+        }
+    }
+}
