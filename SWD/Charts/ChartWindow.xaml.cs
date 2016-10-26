1035d1e6a5b5a9ad196df72886423c5c4d402e61
ZNTK <michalszyluk1993@gmail.com>
2016-10-26 23:10:16 +0200

    Klasyfikacja metoda k-nn 1 //wyglad dla pierwszej metody z zadania

--- SWD/Charts/ChartWindow.xaml.cs
+++ SWD/Charts/ChartWindow.xaml.cs
@@ -24,12 +24,14 @@ namespace SWD.Charts
     public partial class ChartWindow : Window
     {        
         public List<string> columnBinding { get; set; }
+        public List<string> metrykaBinding { get; set; }
         public Model.Table mainTable;
         public ChartWindow(Model.Table table)
         {
             InitializeComponent();
             mainTable = table;
             columnBinding = DataTableService.GetColumnHeadersAsList(table);
+            metrykaBinding = new List<string>() { "odległość Euklidesowa", "metryka Manhattan", "nieskończoność", "Mahalanobisa" };
             DataContext = this;
         }
 
@@ -50,6 +52,26 @@ namespace SWD.Charts
         private void buttonClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void buttonRozwinKlasyfikacje_Click(object sender, RoutedEventArgs e)
+        {
+            gridKlasyfikacja.Visibility = Visibility.Visible;
+            buttonRozwinKlasyfikacje.Visibility = Visibility.Collapsed;
+            labelWartoscX.Content = comboBoxFirstColumn.SelectedItem;
+            labelWartoscY.Content = comboBoxSecondColumn.SelectedItem;
+        }
+
+        private void buttonZwin_Click(object sender, RoutedEventArgs e)
+        {
+            buttonRozwinKlasyfikacje.Visibility = Visibility.Visible;
+            gridKlasyfikacja.Visibility = Visibility.Collapsed;
+        }
+
+        private void buttonKlasyfikuj_Click(object sender, RoutedEventArgs e)
+        {
+            //tu ta cala akcja
+
         }
     }
 }
