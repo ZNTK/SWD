f39aa0186df2789e7a9f35461f8340383d32d891
rplo <rlo@tak.nie>
2016-12-23 18:56:59 +0100

    Kmeans

---
+++ SWD/KMeans/KMeansWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SWD.Model;
+using SWD.Services;
+
+namespace SWD.KMeans
+{
+    /// <summary>
+    /// Interaction logic for KMeansWindow.xaml
+    /// </summary>
+    public partial class KMeansWindow : Window
+    {
+        public SWD.Model.Table mainTable;
+        public KMeansWindow(SWD.Model.Table table)
+        {
+            InitializeComponent();
+            mainTable = table;
+            comboBoxColumn.ItemsSource = table.Headers.Cells.Select(c => c.Value).ToList();
+        }
+
+
+        private void buttonGroup_Click(object sender, RoutedEventArgs e)
+        {
+            int sectionCount;
+            if (!Int32.TryParse(textBoxSectionCount.Text, out sectionCount))
+                MessageBox.Show("error");
+            else
+            {
+               
+                mainTable = KMeansService.Group(mainTable, comboBoxColumn.SelectedIndex, sectionCount);
+                this.Close();
+            }
+
+        }
+    }
+}
---
+++ SWD/KMeans/Model/RowClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD.KMeans.Model
+{
+    public class RowClass
+    {
+        public int RowIndex;
+        public int ClassIndex;
+
+        public RowClass(int RowIndex, int ClassIndex)
+        {
+            this.RowIndex = RowIndex;
+            this.ClassIndex = ClassIndex;
+        }
+
+        public RowClass()
+        { }
+    }
+}
